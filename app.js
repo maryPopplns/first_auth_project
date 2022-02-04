f1e931946820f4490b5c36e54895728419352db2
Spencer Knight <spencer.knightusn@gmail.com>
2022-02-04 09:40:37 -0700

    added bcrypt

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
@@ -40,25 +41,48 @@ passport.use(
       if (!user) {
         return done(null, false, { message: 'Incorrect username' });
       }
-      if (user.password !== password) {
-        return done(null, false, { message: 'Incorrect password' });
-      }
-      return done(null, user);
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user);
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: 'Incorrect password' });
+        }
+      });
     });
   })
 );
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.get('/', (req, res) => res.render('index'));
 app.get('/signup', (req, res) => res.render('signUp'));
 app.post('/signup', (req, res, next) => {
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password,
-  }).save((err) => {
+  bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
     if (err) {
       return next(err);
     }
-    res.redirect('/');
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword,
+    }).save((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/');
+    });
   });
 });
 app.post(
@@ -68,5 +92,9 @@ app.post(
     failureRedirect: '/',
   })
 );
+app.get('/logOut', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
 
 app.listen(3000, () => console.log('app listening on port 3000!'));
